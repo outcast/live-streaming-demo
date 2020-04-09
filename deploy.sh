d5b71052b1184d0f1478abe1e845209fece317f6
James Jones <james.jones@ibm.com>
2020-04-08 21:23:35 -0400

    add sleep to wait for machines to come up

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 #Deploy Origin Server
 pushd ./terraform
+terraform init
 make plan
 make apply
 popd
-#ansible-playbook -e "ansible_user=root" -i ./configs/ansible/nodes.conf ./playbooks/ingress-node.playbook
-#ansible-playbook -e "ansible_user=root" -i ./configs/ansible/nodes.conf ./playbooks/worker-node.playbook
-#ansible-playbook -e "ansible_user=root" -i ./configs/ansible/nodes.conf ./playbooks/lb-node.playbook
+echo Sleeping so servers can boot....
+sleep 20
 ansible-playbook -e "ansible_user=root" -i ./configs/ansible/nodes.conf ./playbooks/*-node.playbook
